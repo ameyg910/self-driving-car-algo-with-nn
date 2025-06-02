a7cdc4c1f8ffbcdb5af0a420fba4e9728939e1e6
ameyg910 <ameygupta880@gmail.com>
2025-06-02 22:34:50 +0530

    Car driving mechanics done

--- car.js
+++ car.js
@@ -6,9 +6,10 @@ class Car{
         this.height=height; 
         
         this.speed=0,
-        this.acceleration=0.4, 
-        this.maxSpeed=6,
-        this.friction=0.1
+        this.acceleration=0.2, 
+        this.maxSpeed=3,
+        this.friction=0.05
+        this.angle=0; 
         this.controls = new Controls();
     }
     update(){
@@ -30,16 +31,34 @@ class Car{
         if(this.speed<0){
             this.speed+=this.friction; 
         }
-        this.y-=this.speed; 
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0; 
+        }
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+        if(this.controls.left){
+            this.angle+=0.03*flip;
+        }
+        if(this.controls.right){
+            this.angle-=0.03*flip; 
+        }
+    }
+        this.x-=Math.sin(this.angle)*this.speed; 
+        this.y-=Math.cos(this.angle)*this.speed;  
     }
     draw(ctx){
-        ctx.beginPath();    
+        ctx.save();    
+        ctx.translate(this.x, this.y); 
+        ctx.rotate(-this.angle); 
+        ctx.beginPath(); 
         ctx.rect(
-            this.x=this.width/2,
-            this.y-this.height/2, 
+            -this.width/2,
+            -this.height/2, 
             this.width, 
             this.height
         );
        ctx.fill(); 
+
+       ctx.restore(); 
     }
 }
