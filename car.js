c0b6219cdfe11a460cfb6032350a917d706e7b42
ameyg910 <ameygupta880@gmail.com>
2025-06-02 15:22:33 +0530

    control mechanics

---
+++ car.js
@@ -0,0 +1,45 @@
+class Car{
+    constructor(x, y, width, height){
+        this.x=x; 
+        this.y=y; 
+        this.width=width; 
+        this.height=height; 
+        
+        this.speed=0,
+        this.acceleration=0.4, 
+        this.maxSpeed=6,
+        this.friction=0.1
+        this.controls = new Controls();
+    }
+    update(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration; 
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration; 
+        }
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed; 
+        }
+        if(this.speed<-this.maxSpeed){
+            this.speed=-this.maxSpeed; 
+        }
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction; 
+        }
+        this.y-=this.speed; 
+    }
+    draw(ctx){
+        ctx.beginPath();    
+        ctx.rect(
+            this.x=this.width/2,
+            this.y-this.height/2, 
+            this.width, 
+            this.height
+        );
+       ctx.fill(); 
+    }
+}
