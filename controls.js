8fbe9022dbc0dddcdc6c1475fcc931773f8643b1
ameyg910 <ameygupta880@gmail.com>
2025-06-05 14:27:37 +0530

    traffic simulation

--- controls.js
+++ controls.js
@@ -1,43 +1,51 @@
 class Controls{
-    constructor(){
+    constructor(type){
         this.forward=false;
-        this.left=false; 
-        this.right=false; 
-        this.reverse=false; 
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.#addKeyboardListners(); 
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
-    #addKeyboardListners(){
+
+    #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=true; 
-                    break; 
+                    this.left=true;
+                    break;
                 case "ArrowRight":
-                    this.right=true; 
-                    break; 
+                    this.right=true;
+                    break;
                 case "ArrowUp":
-                    this.forward=true; 
-                    break; 
+                    this.forward=true;
+                    break;
                 case "ArrowDown":
                     this.reverse=true;
-                    break; 
+                    break;
             }
         }
         document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=false; 
-                    break; 
+                    this.left=false;
+                    break;
                 case "ArrowRight":
-                    this.right=false; 
-                    break; 
+                    this.right=false;
+                    break;
                 case "ArrowUp":
-                    this.forward=false; 
-                    break; 
+                    this.forward=false;
+                    break;
                 case "ArrowDown":
                     this.reverse=false;
-                    break; 
+                    break;
             }
         }
     }
